8026944530829d2c8dfcc151de4d9dff6d566178
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2016-12-15 21:11:24 +0530

    added domain module with support for new domain creation and kicking off of default concept, intent and firing search jobs for new domain

--- server/domains/domainEntity.js
+++ server/domains/domainEntity.js
@@ -3,28 +3,17 @@ const mongoose = require('mongoose');
 /* beautify ignore:start */
 /* beautify preserve:start */
 const schema = new mongoose.Schema({
-	domain: String,
-	description: String,
-	user: String,
-	creationDate: String,
-  concepts:Number,
-  image:String,
-	intents:[
-    {intent:String,docs: Number},
-    {intent:String,docs: Number},
-    {intent:String,docs: Number},
-    {intent:String,docs: Number},
-    {intent:String,docs: Number}
-	 ]
-}, {collection: 'domain', versionKey: false});
+	name: { type: String, required: true, unique: true },
+	description: { type: String },
+	domainImgURL: { type: String },
+	createdBy: { type: String, default: 'admin' },
+	createdOn: { type: Date, default: Date.now }
+}, {collection: 'domains', versionKey: false});
 
-//schema.index({url: 1}, {unique: true});
-
-const model = mongoose.model('domain', schema);
+const model = mongoose.model('domains', schema);
 
 module.exports = {
-	domain: model
+	DomainModel: model
 };
-
 /* beautify preserve:end */
 /* beautify ignore:end */
