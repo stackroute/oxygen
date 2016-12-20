1095ddde8cb03df312d9c1f84c268f2638d88ace
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2016-12-20 14:42:29 +0530

    Added ASCII art for services, so that when they run we know who is who

--- server/docintentparser.service.js
+++ server/docintentparser.service.js
@@ -7,41 +7,54 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 function setupMongooseConnections() {
- mongoose.connect(config.MONGO_URL);
+  mongoose.connect(config.MONGO_URL);
 
- mongoose.connection.on('connected', function() {
-   logger.debug('Mongoose is now connected to ', config.MONGO_URL);
- });
+  mongoose.connection.on('connected', function() {
+    logger.debug('Mongoose is now connected to ', config.MONGO_URL);
+  });
 
- mongoose.connection.on('error', function(err) {
-   logger.error('Error in Mongoose connection: ', err);
- });
+  mongoose.connection.on('error', function(err) {
+    logger.error('Error in Mongoose connection: ', err);
+  });
 
- mongoose.connection.on('disconnected', function() {
-   logger.debug('Mongoose is now disconnected..!');
- });
+  mongoose.connection.on('disconnected', function() {
+    logger.debug('Mongoose is now disconnected..!');
+  });
 
- process.on('SIGINT', function() {
-   mongoose.connection.close(function() {
-     logger.info(
-       'Mongoose disconnected on process termination'
-     );
-     process.exit(0);
-   });
- });
+  process.on('SIGINT', function() {
+    mongoose.connection.close(function() {
+      logger.info(
+        'Mongoose disconnected on process termination'
+      );
+      process.exit(0);
+    });
+  });
+}
+
+function welcome() {
+  let motdFile = path.resolve(__dirname, '.intent.motd');
+  const fs = require('fs');
+  if (fs.existsSync(motdFile)) {
+    let msg = fs.readFileSync(motdFile, 'utf-8');
+    process.stdout.write('\n' + msg + '\n');
+  } else {
+    process.stdout.write('\n=========== Oxygen Crawler ===========\n');
+  }
 }
 
 let startIntentParserEngine = function() {
- try {
-   //Any pre-requisites for running the engine
-   setupMongooseConnections();
+  try {
+    welcome();
 
-   logger.info("Starting doc intentParser engine..!");
+    //Any pre-requisites for running the engine
+    setupMongooseConnections();
 
-   intentParserEngine.startIntentParser();
- } catch (err) {
-   logger.error("Caught error in running doc intentParser engine: ", err);
- }
+    logger.info("Starting doc intentParser engine..!");
+
+    intentParserEngine.startIntentParser();
+  } catch (err) {
+    logger.error("Caught error in running doc intentParser engine: ", err);
+  }
 }
 
 startIntentParserEngine();
