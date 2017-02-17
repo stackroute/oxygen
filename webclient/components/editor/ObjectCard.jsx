64d59b995861a75cf6cfc8d321cdfa91c69edddd
shaktiram <shakthi16277@gmail.com>
2017-02-17 18:18:02 +0530

    update ui

--- webclient/components/editor/ObjectCard.jsx
+++ webclient/components/editor/ObjectCard.jsx
@@ -17,7 +17,7 @@ const styles = {
     }
 };
 
-export default class SubjectCard extends React.Component {
+export default class ObjectCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
