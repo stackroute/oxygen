866604941eab0a5b83ec13805bc0ef776543b4c7
sathakowsik <sathakowsik@gmail.com>
2017-02-17 18:54:34 +0530

    Loading predicate details in card

--- webclient/components/editor/SubjectNode.jsx
+++ webclient/components/editor/SubjectNode.jsx
@@ -387,7 +387,8 @@ export default class SubjectNode extends React.Component {
                 selectedPredicateDetails['properties'] = response;
 
                 this.setState({
-                  selectedPredicateDetails: selectedPredicateDetails
+                  selectedPredicateDetails: selectedPredicateDetails,
+                  predicateCardJsx: true
                   });
             }
         });
