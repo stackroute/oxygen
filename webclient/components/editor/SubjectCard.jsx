53e231f6109c676eba758517d6fd7e904f1ab110
sathakowsik <sathakowsik@gmail.com>
2017-02-20 15:04:25 +0530

    updated loading of subject cards

--- webclient/components/editor/SubjectCard.jsx
+++ webclient/components/editor/SubjectCard.jsx
@@ -86,12 +86,10 @@ export default class SubjectCard extends React.Component {
                         marginLeft: '50%'
                     }}/>
                     <CardActions>
-                        <DropDownMenu value={this.state.value} onChange={this.handleChange} style={styles.customWidth} autoWidth={false}>
-                            <MenuItem value={0} primaryText='Select Type'/>
-                            <MenuItem value={1} primaryText='Intent'/>
-                            <MenuItem value={2} primaryText='Concept'/>
-                            <MenuItem value={3} primaryText={this.state.subjectCard['type']}/>
-                        </DropDownMenu>
+                        
+                        <TextField floatingLabelText='Type' value={this.state.subjectCard['type']} style={{
+                            fullWidth: 'true'
+                        }}/>
                         <TextField floatingLabelText='Name' value={this.state.subjectCard['name']} style={{
                             fullWidth: 'true'
                         }}/>
