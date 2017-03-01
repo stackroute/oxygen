2a877096d97903f2d4a0c2223be945a36fc44ec8
jagatheeswaran <jagatheeswaran.t.s@gmail.com>
2017-03-01 08:35:42 +0530

    Change in landing page and mind map graph

--- webclient/components/home/home.jsx
+++ webclient/components/home/home.jsx
@@ -18,9 +18,12 @@ const styles = {
       textAlign:'center',
       fontWeight: 'normal'
     },
+    h3:{
+      textAlign:'center'
+    },
     p: {
         marginTop: 0,
-        textAlign:'justify'
+        textAlign:'center'
     },
     link:{
       color: 'white',
@@ -31,7 +34,8 @@ const styles = {
       marginLeft: 50,
       marginRight: 50,
       fontFamily: 'sans-serif'
-    }
+    },
+    img:{margin:32}
 };
 
 export default class Home extends React.Component {
@@ -54,45 +58,54 @@ export default class Home extends React.Component {
         <div style={styles.container}>
           <Row md={12} lg={12} sm={12} xs={12}>
             <Col md={4} lg={4} sm={12} xs={12}>
-              <h3>Content Suggestion</h3>
+              <center><img style={styles.img} src='../../assets/images/landingpage/suggestion.png'></img></center>
+              <h3 style={styles.h3}>Content Suggestion</h3>
               <p style={styles.p}>Suggests content for specified purpose and knowledge level.</p>
             </Col>
             <Col md={4} lg={4} sm={12} xs={12}>
-              <h3>Indexing</h3>
+              <center><img src='../../assets/images/landingpage/indexing.png'></img></center>
+              <h3 style={styles.h3}>Indexing</h3>
               <p style={styles.p}>Indexes the content to its usage based on purpose and also based on ontology.</p>
             </Col>
             <Col md={4} lg={4} sm={12} xs={12}>
-              <h3>Build and train ontology</h3>
+              <center><img style={styles.img} src='../../assets/images/landingpage/buildandtrain.png'></img></center>
+              <h3 style={styles.h3}>Build and train ontology</h3>
               <p style={styles.p}>Build and train the ontology of a particular domain using Ontology Trainer.</p>
             </Col>
           </Row>
           <br/>
           <Row md={12} lg={12} sm={12}>
             <Col md={4} lg={4} sm={12} xs={12}>
-              <h3>Visualize ontology</h3>
+              <center><img style={styles.img} src='../../assets/images/landingpage/visualize.png'></img></center>
+              <h3 style={styles.h3}>Visualize ontology</h3>
               <p style={styles.p}>View the ontology or the structure of a particular domain an it's corresponding resources.</p>
             </Col>
             <Col md={4} lg={4} sm={12} xs={12}>
-              <h3>Auto Sourcing</h3>
+              <center><img style={styles.img} src='../../assets/images/landingpage/autosource.png'></img></center>
+              <h3 style={styles.h3}>Auto Sourcing</h3>
               <p style={styles.p}>Documents of a particular domain are sourced automatically when that domain is added.</p>
             </Col>
             <Col md={4} lg={4} sm={12} xs={12}>
-              <h3>Highly Scalable</h3>
+              <center><img style={styles.img} src='../../assets/images/landingpage/scalability.png'></img></center>
+              <h3 style={styles.h3}>Highly Scalable</h3>
               <p style={styles.p}>Oxygen is capable of handling high data rate and also huge number of web documents.</p>
             </Col>
           </Row>
           <br/>
           <Row>
             <Col md={4} lg={4} sm={12} xs={12}>
-              <h3>Focussed Crawling</h3>
+              <center><img style={styles.img} src='../../assets/images/landingpage/crawling.png'></img></center>
+              <h3 style={styles.h3}>Focussed Crawling</h3>
               <p style={styles.p}>Captures the meaning of web documents by extracting the semantic importance of the words in them.</p>
             </Col>
             <Col md={4} lg={4} sm={12} xs={12}>
-              <h3>Feed URLs manually</h3>
+              <center><img style={styles.img} src='../../assets/images/landingpage/feed.png'></img></center>
+              <h3 style={styles.h3}>Feed URLs manually</h3>
               <p style={styles.p}>Manual add URLs of the documents that you wish to add.</p>
             </Col>
             <Col md={4} lg={4} sm={12} xs={12}>
-              <h3>Export data to RDF</h3>
+              <center><img style={styles.img} src='../../assets/images/landingpage/export.png'></img></center>
+              <h3 style={styles.h3}>Export data to RDF</h3>
               <p style={styles.p}>Easily export data to RDF, which is the widely used data schema.</p>
             </Col>
           </Row>
